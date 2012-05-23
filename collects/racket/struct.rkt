57f4c07591b4946581e5a1fe80ec7675208d769e
Vincent St-Amour <stamourv@racket-lang.org>
2012-05-23 10:47:42 -0400

    Add a guard for prop:gen:equal+hash.

--- collects/racket/struct.rkt
+++ collects/racket/struct.rkt
@@ -8,9 +8,22 @@
 (provide gen:equal+hash)
 
 (define-values (prop:gen:equal+hash equal+hash? gen:equal+hash-acc)
-  (make-struct-type-property 'prop:gen:equal+hash
-                             #f ;; TODO have something
-                             (list (cons prop:equal+hash vector->list))))
+  (make-struct-type-property
+   'prop:gen:equal+hash
+   (lambda (v si)
+     (unless (and (vector? v)
+                  (= 3 (vector-length v))
+                  (procedure? (vector-ref v 0))
+                  (procedure-arity-includes? (vector-ref v 0) 3)
+                  (procedure? (vector-ref v 1))
+                  (procedure-arity-includes? (vector-ref v 1) 2)
+                  (procedure? (vector-ref v 2))
+                  (procedure-arity-includes? (vector-ref v 2) 2))
+       (raise-type-error 'guard-for-prop:gen:equal+hash
+                         "vector of three procedures (arities 3, 2, 2)"
+                         v))
+     v)
+   (list (cons prop:equal+hash vector->list))))
 
 (define-generics (equal+hash gen:equal+hash prop:gen:equal+hash equal+hash?
                              #:defined-table dummy
