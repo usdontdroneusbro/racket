d6dd8d9e1d9d9f714e2ecca5ea0d8971da4f1bcf
Asumu Takikawa <asumu@ccs.neu.edu>
2013-08-05 14:10:45 -0400

    Fix a private/augment method issue

--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/unit-tests/class-tests.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/unit-tests/class-tests.rkt
@@ -1043,7 +1043,13 @@
      (: c% (Class (field [x String])))
      (define c%
        (class object% (super-new)
-         (field [x : Integer 5]))))))
+         (field [x : Integer 5]))))
+
+   ;; fails, but make sure it's not an internal error
+   (check-err #:exn #rx"Cannot apply expression of type Any"
+     (class object% (super-new)
+            (define/pubment (foo x) 0)
+            (define/public (g x) (foo 3))))))
 
 (define-go class-tests)
 
