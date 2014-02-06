82c51a9a972e690183d9e150e92093c3fc91dc2b
Asumu Takikawa <asumu@ccs.neu.edu>
2014-02-06 15:41:23 -0500

    Improve timer% contract

--- pkgs/gui-pkgs/gui-lib/mred/contract/timer.rkt
+++ pkgs/gui-pkgs/gui-lib/mred/contract/timer.rkt
@@ -6,6 +6,9 @@
 
 (define timer%/c
  (class/c
+  (init [notify-callback (-> any)]
+        [interval (or/c (integer-in 0 1000000000) #f)]
+        [just-once? any/c])
   (interval (->m (integer-in 0 1000000000)))
   (notify (->m any))
   (override
